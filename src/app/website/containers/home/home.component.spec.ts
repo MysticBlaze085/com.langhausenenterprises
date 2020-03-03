8b8790a4fd891026e293b16f44ed569eff85c07c
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-03-02 21:07:00 -0600

    removed error schemas

--- src/app/website/containers/home/home.component.spec.ts
+++ src/app/website/containers/home/home.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { NgbNav } from '@ng-bootstrap/ng-bootstrap';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -9,6 +10,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
       declarations: [HomeComponent, NgbNav],
     }).compileComponents();
   }));
