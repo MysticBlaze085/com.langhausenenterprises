afdb4588da0605a4ab67b77afec24c2894196377
MysticBlaze085 <MysticBlaze085@users.noreply.github.com>
2020-07-24 16:33:18 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyB5vFwie56xsAdifdrnuOPn3j-ZXSIJ7Y4',
-    authDomain: 'comle-74ecb.firebaseapp.com',
-    databaseURL: 'https://comle-74ecb.firebaseio.com',
-    projectId: 'comle-74ecb',
-    storageBucket: 'comle-74ecb.appspot.com',
-    messagingSenderId: '950207085182',
-    appId: '1:950207085182:web:26c1183f0df9a61fbb762f',
-    measurementId: 'G-1P2705403R'
+    //firebase details have to go in here
   }
 };
 
